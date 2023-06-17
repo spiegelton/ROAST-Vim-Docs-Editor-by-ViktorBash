2c5f270ff8c97b0b22b8f63ebc136095a098424f
ViktorBash <basharkevichv@gmail.com>
2023-06-17 00:43:31 -0400

    Removed tabs permission and autoreload on trial/paid

--- background.js
+++ background.js
@@ -3,30 +3,6 @@ importScripts('dist/ExtPay.js');
 var extpay = ExtPay('vim-for-docs'); // Extension ID
 extpay.startBackground();  // Required, have extpay running in the background
 
-// When user pays, refresh google docs so they can use the extension
-extpay.onPaid.addListener(() => {
-	// Reload the google doc after a user just paid
-    chrome.tabs.query({
-        url: "https://docs.google.com/*" 
-    }, function(tabs) {
-        for (let i = 0; i < tabs.length; i++) {
-            chrome.tabs.reload(tabs[i].id);
-        }
-    })
-});
-
-// When user pays, refresh google docs so they can use the extension
-extpay.onTrialStarted.addListener(() => {
-	// Reload the google doc after a user just started their trial
-    chrome.tabs.query({
-        url: "https://docs.google.com/*" 
-    }, function(tabs) {
-        for (let i = 0; i < tabs.length; i++) {
-            chrome.tabs.reload(tabs[i].id);
-        }
-    })
-});
-
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
         // When the extension is installed for the first time, prompt them to the popup HTML page
