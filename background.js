749408e2ec74aa81bad95ea0992c3c4cd4ac3d52
ViktorBash <basharkevichv@gmail.com>
2023-07-14 10:38:42 -0400

    Added Vimium warning (requires management permission)

--- background.js
+++ background.js
@@ -1,18 +1,28 @@
-importScripts('dist/ExtPay.js');
+importScripts("dist/ExtPay.js");
 
-var extpay = ExtPay('vim-for-docs'); // Extension ID
-extpay.startBackground();  // Required, have extpay running in the background
+var extpay = ExtPay("vim-for-docs"); // Extension ID
+extpay.startBackground(); // Required, have extpay running in the background
 
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        // When the extension is installed for the first time, prompt them to the popup HTML page
-        // this opens on a full tab screen instead of a window
-        chrome.tabs.create({url: "popup.html"});
-        
-    }
-    // else if(details.reason == "update"){
-    //     // Extension updated
-    //     var thisVersion = chrome.runtime.getManifest().version;
-    //     console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
-    // }
+chrome.runtime.onInstalled.addListener(function (details) {
+	if (details.reason == "install") {
+		// When the extension is installed for the first time, prompt them to the popup HTML page
+		// this opens on a full tab screen instead of a window
+		chrome.tabs.create({ url: "popup.html" });
+
+		// Check if vimium exists, and if it does, then open the vimium warning page
+		chrome.management.getAll(function (chromeExtensionList) {
+			for (let i = 0; i < chromeExtensionList.length; i++) {
+				if (chromeExtensionList[i].id == "dbepggeogbaibhgnhhndojpepiihcmeb") {
+					chrome.tabs.create({ url: "vimium_warning.html" });
+					break;
+				}
+			}
+		})
+	}
+
+	// else if(details.reason == "update"){
+	//     // Extension updated
+	//     var thisVersion = chrome.runtime.getManifest().version;
+	//     console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+	// }
 });
